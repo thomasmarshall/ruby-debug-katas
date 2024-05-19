444403d3f35a3d3eea13ce598c38a6fd84e82e72
Thomas Marshall <thomas@thomasmarshall.com>
2024-05-19 20:23:11 +0100

    Add quit command exercise

---
+++ exercises/01_quit.rb
@@ -0,0 +1,6 @@
+require "debug"
+
+# Welcome to the `quit` kata!
+binding.break # Type `quit` or `q` and then `Y` to finish the kata.
+              # You can also use `Ctrl-D`.
+              # You can also use `quit!` or `q!` to skip the prompt.

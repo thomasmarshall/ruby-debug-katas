393ad27a8d611d431c5ac8dc7c4400973faca98c
Thomas Marshall <thomas@thomasmarshall.com>
2024-05-19 20:23:24 +0100

    Add whereami command exercise

---
+++ exercises/02_whereami.rb
@@ -0,0 +1,7 @@
+require "debug"
+
+# Welcome to the `whereami` kata! You'll need to know the following commands:
+# - quit
+binding.break # Type `whoops` (an invalid command) to lose yourself.
+              # Type `whereami` to find yourself again.
+              # Quit to finish the kata.
